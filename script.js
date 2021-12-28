3384afd457a7cfa07587f4d557387f42197dcb53
Sujan <invadorshadow@gmail.com>
2021-12-28 09:54:14 +0545

    First Version

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 // // document.querySelector(".player-name").innerText = playerName;
 // $(".player-name").text(playerName);
 
+
+function gameStart(){
+        
 //Random Number Generator Function for Question 
 function randomNumberGenerator(){
     var number = Math.floor((Math.random()*99)+1);
@@ -18,7 +21,7 @@ function random_number_generator_for_wrong_answer(){
     var add_or_substract = Math.floor(Math.random()*20 + 10);
     return(add_or_substract);
 }
-
+ 
 var firstNumber = randomNumberGenerator();
 var secondNumber = randomNumberGenerator();
 var correctAnswer = firstNumber * secondNumber;
@@ -61,4 +64,69 @@ while (whileEnd !== 1){
     console.log("Correct Answer: " + correctAnswer);
 }
 
+//blow is the working code
+
+// $(".answer").click(function(){
+//     $(".answer").removeClass("selected hover-cancel"); //removes any previously selected answer so that only one could be selected
+//     $(this).addClass("selected hover-cancel"); // add css to selected answer
+//     var selectedElement = this;
+//     return(selectedElement);
+//     // console.log(selectedElement);
+
+// });
+
+
+function checkAnswer(selectedElement){
+
+    $(".answer").click(function(){
+        $(".answer").removeClass("selected hover-cancel"); //removes any previously selected answer so that only one could be selected
+        $(this).addClass("selected hover-cancel"); // add css to selected answer
+        selectedElement = this;
+        return(selectedElement);
+        
+    });
+
+    $(".answer-check-btn").click(function(){
+
+        $(".answer").unbind();
+        var selectedOption = $(selectedElement).text();
+        
+        if(selectedOption == correctAnswer){
+            $(selectedElement).removeClass("selected").addClass("correct-selected");
+            $(".answer-check-btn").addClass("answer-check-right").text("Correct Answer");
+            console.log("Selected Option:  "+selectedOption);
+            console.log("Correct Answer " + correctAnswer)
+    
+        }
+
+        else if(selectedOption != correctAnswer){
+            
+            $(selectedElement).removeClass("selected").addClass("incorrect-selected");
+            $(".answer-check-btn").addClass("answer-check-wrong").text("Wrong Answer");
+            console.log("Selected Option:  "+selectedOption);
+            console.log("Correct Answer " + correctAnswer)
+           
+        }
+        
+        $(".next-question").removeClass("hide");
+
+    })
+}
+
+    checkAnswer(); 
+
+}
+
+$(".next-question").click(function(){
+    // $(".selected").removeClass("selected");
+    $(".correct-selected").removeClass("correct-selected");
+    $(".incorrect-selected").removeClass("incorrect-selected");
+    $(".hover-cancel").removeClass("hover-cancel");
+    $(".answer-check-right").removeClass("answer-check-right");
+    $(".answer-check-wrong").removeClass("answer-check-wrong");
+    $(".next-question").addClass("hide");
+    gameStart();
+})
+
+gameStart();
 
